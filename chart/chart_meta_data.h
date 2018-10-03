7d5296f49308bbc4da9631d79b942e51d322721f
masaka <masaka@mz-kb.com>
2018-10-03 10:02:34 +0900

    feat: add ChartMetaData accessor

--- chart/chart_meta_data.h
+++ chart/chart_meta_data.h
@@ -11,4 +11,24 @@ private:
 
 public:
     ChartMetaData(const std::string & filename);
+
+    std::size_t size() const
+    {
+        return m_params.size();
+    }
+
+    std::size_t count(const std::string & key) const
+    {
+        return m_params.count(key);
+    }
+
+    const std::string & at(const std::string & key) const
+    {
+        return m_params.at(key);
+    }
+
+    std::string & operator[](const std::string & key)
+    {
+        return m_params[key];
+    }
 };
