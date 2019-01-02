6094d5af7b00916a575abed2b103838423eb5037
masaka <masaka@mz-kb.com>
2019-01-03 05:10:04 +0900

    refactor: move full functionarities of ChartMetaData to Chart class

--- src/chart/chart.h
+++ src/chart/chart.h
@@ -1,18 +1,33 @@
 #pragma once
 
 #include <string>
+#include <unordered_map>
+#include <fstream>
 
 #include "chart_meta_data.h"
 #include "chart_body.h"
 
+// Chart (header)
 class Chart
 {
 private:
+    bool m_isUTF8;
+
+protected:
     const std::string m_filename;
     const std::string m_fileDirectoryPath;
+    std::ifstream m_ifs;
 
 public:
-    const ChartMetaData meta;
-    const ChartBody body;
-    Chart(const std::string & filename);
+    // Chart meta data
+    const std::unordered_map<std::string, std::string> metaData;
+
+    explicit Chart(const std::string & filename, bool keepFileStreamOpen = false);
+
+    std::string toString() const;
+
+    bool isUTF8() const
+    {
+        return m_isUTF8;
+    }
 };
