809a4e9af5508d5e18408444b2354e1c02daa588
masaka <masaka@mz-kb.com>
2019-01-22 03:59:14 +0900

    feat: add getting editable lanes from EditableChart

--- src/chart/editable_chart.h
+++ src/chart/editable_chart.h
@@ -7,4 +7,19 @@ class EditableChart : public PlayableChart
 {
 public:
     EditableChart(const std::string & filename) : PlayableChart(filename, true) {}
-}
+
+    Lane<BTNote> & btLane(std::size_t idx)
+    {
+        return m_btLanes.at(idx);
+    }
+
+    Lane<FXNote> & fxLane(std::size_t idx)
+    {
+        return m_fxLanes.at(idx);
+    }
+
+    Lane<LaserNote> & laserLane(std::size_t idx)
+    {
+        return m_laserLanes.at(idx);
+    }
+};
