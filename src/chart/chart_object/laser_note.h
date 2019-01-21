2c7c266403a55d3b42ac25a9c3c564854cf5d47d
masaka <masaka@mz-kb.com>
2019-01-21 23:36:24 +0900

    feat: add LaserNote::isSlam()

--- src/chart/chart_object/laser_note.h
+++ src/chart/chart_object/laser_note.h
@@ -7,7 +7,13 @@ struct LaserNote final : public AbstractNote
 public:
     double startX;
     double endX;
+
     LaserNote(Measure length, double startX, double endX, Measure posForJudgmentAlignment = 0, bool halvesCombo = false);
+
+    bool isSlam()
+    {
+        return length <= UNIT_MEASURE / 32;
+    }
 
     static int charToLaserX(unsigned char c);
 };
