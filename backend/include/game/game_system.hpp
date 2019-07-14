5d740af339f598e5d91a5b13df96bbfe558840a3
masaka <masaka@mz-kb.com>
2019-07-14 17:51:58 +0900

    Receive GameSettings in GameSystem ctor

--- backend/include/game/game_system.hpp
+++ backend/include/game/game_system.hpp
@@ -1,9 +1,13 @@
 #pragma once
+#include "game/game_settings.hpp"
 
 class GameSystem
 {
+private:
+    const GameSettings m_settings;
+
 public:
-    GameSystem() = default;
+    explicit GameSystem(const GameSettings & settings);
     ~GameSystem() = default;
 
     void update();
--- backend/src/game/game_system.cpp
+++ backend/src/game/game_system.cpp
@@ -1,5 +1,10 @@
 #include "game/game_system.hpp"
 
+GameSystem::GameSystem(const GameSettings & settings)
+    : m_settings(settings)
+{
+}
+
 void GameSystem::update()
 {
 }
